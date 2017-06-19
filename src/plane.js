43081695dd28bd46096cacda4fa9fb031de48e01
mxz96102 <mxz96102@qq.com>
2017-06-19 16:33:06 +0800

    create main 3D object plane

---
+++ src/plane.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Doming on 2017/6/19.
+ */
+import Sprite from 'sprite';
+import Other from 'other';
+
+export default class Plane extends Sprite {
+  initialize(params) {
+    super.initialize.apply(this, [params]);
+  }
+
+  update() {
+    super.update.apply(this);
+    this.updateF();
+    return this;
+  }
+
+  updateS() {
+    this.el.style.width = Other.fixed0(this.width) + 'px';
+    this.el.style.height = Other.fixed0(this.height) + 'px';
+    return this;
+  }
+
+  updateF() {
+    if (!this.__flt) return this;
+
+    let _flt = '';
+
+    for (let i in this.__flt) {
+      _flt += (this.__flt[i] !== '' ? (i + '(' + this.__flt[i].join(',') + ')') : '');
+    }
+
+    if (_flt !== '') this.el.style[this.prefix + 'Filter'] = _flt;
+
+    return this;
+  }
+
+  filter(obj) {
+    this.__flt = obj;
+    return this;
+  }
+}
