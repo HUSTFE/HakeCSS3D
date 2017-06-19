1632a14b9bd2791b6ec4b177149641197ed58c15
mxz96102 <mxz96102@qq.com>
2017-06-19 17:13:19 +0800

    finished 1st version need to test.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+import Box from 'box';
+import Camera from 'camera';
+import Color from 'color';
+import Plane from 'plane';
+import Skybox from 'skybox';
+import Sprite from 'sprite';
+import Stage from 'stage';
+import TObject from 'TObject';
 
 export default class CSS3Renderer {
   constructor() {
-    this.__version = '0.0.0';
+    this.__version = '0.0.1';
+    this.color = Color;
+    this.Object = TObject;
+    this.Sprite = Sprite;
+    this.Camera = Camera;
+    this.Box = Box;
+    this.Plane = Plane;
+    this.Skybox = Skybox;
+    this.Stage = Stage;
   }
 }
